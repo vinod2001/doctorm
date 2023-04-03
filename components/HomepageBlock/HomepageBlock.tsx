00acff06b138f48dd200c83be14f6c33dae0eff5
anooppius-bbcommerce <anoop.pius@bigbangcommerce.com>
2023-04-04 01:15:31 +0530

    Home page sanity changes

--- components/HomepageBlock/HomepageBlock.tsx
+++ components/HomepageBlock/HomepageBlock.tsx
@@ -22,7 +22,7 @@ export function HomepageBlock({ homePageContent }) {
     translations: {
       contents
     }
-  } = homePageContent;
+  } = homePageContent || { seoDetails: {}, translations: {} };
 
   return (
     <Grid>
