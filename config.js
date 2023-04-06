219a730db644c87c9a1b8ef646399b0494209352
anooppius-bbcommerce <127185335+anooppius-bbcommerce@users.noreply.github.com>
2023-04-06 13:53:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     sanity: {
       projectId: process.env.NEXT_SANITY_STUDIO_API_PROJECT_ID || 'lsciif36',
-      dataset: process.env.NEXT_SANITY_STUDIO_API_DATASET || 'test',
+      dataset: process.env.NEXT_SANITY_STUDIO_API_DATASET || 'production',
       apiVersion: process.env.NEXT_SANITY_STUDIO_API_VERSION || '2021-06-07',
       token: process.env.NEXT_SANITY_STUDIO_API_READ_TOKEN || 'skLeItChoOctyWwqWOuFb3YGEzGeuKKBipXBycgQb8zN5XTjxJrSYrSp0D8E2xGJ36Z4oXpqwRuTHASzchMRnNiasgqp2EOqUQSxNnYtOwNKSV9tYQLEJqSx8I53i6APwqvbtUeqKpUbQcOjDMuQAAFhd99dQdcmZRav7K97RzGFfFza3mJx',
     },
