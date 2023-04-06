f551298c61bee8e6d08ac5ff3a43f9e10b99f9a8
anooppius-bbcommerce <127185335+anooppius-bbcommerce@users.noreply.github.com>
2023-04-06 13:43:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
     sanity: {
-      projectId: process.env.SANITY_STUDIO_API_PROJECT_ID || 'lsciif36',
-      dataset: process.env.SANITY_STUDIO_API_DATASET || 'test',
-      apiVersion: process.env.SANITY_STUDIO_API_VERSION || '2021-06-07',
-      token: process.env.SANITY_STUDIO_API_READ_TOKEN || 'skLeItChoOctyWwqWOuFb3YGEzGeuKKBipXBycgQb8zN5XTjxJrSYrSp0D8E2xGJ36Z4oXpqwRuTHASzchMRnNiasgqp2EOqUQSxNnYtOwNKSV9tYQLEJqSx8I53i6APwqvbtUeqKpUbQcOjDMuQAAFhd99dQdcmZRav7K97RzGFfFza3mJx',
+      projectId: process.env.NEXT_SANITY_STUDIO_API_PROJECT_ID || 'lsciif36',
+      dataset: process.env.NEXT_SANITY_STUDIO_API_DATASET || 'test',
+      apiVersion: process.env.NEXT_SANITY_STUDIO_API_VERSION || '2021-06-07',
+      token: process.env.NEXT_SANITY_STUDIO_API_READ_TOKEN || 'skLeItChoOctyWwqWOuFb3YGEzGeuKKBipXBycgQb8zN5XTjxJrSYrSp0D8E2xGJ36Z4oXpqwRuTHASzchMRnNiasgqp2EOqUQSxNnYtOwNKSV9tYQLEJqSx8I53i6APwqvbtUeqKpUbQcOjDMuQAAFhd99dQdcmZRav7K97RzGFfFza3mJx',
     },
     algoliaIndexName: process.env.NEXT_ALGOLIA_INDEX_NAME || 'saleor_test.web-uae.AED.products',
     algoliaProjectId: process.env.NEXT_ALGOLIA_PROJECT_ID || 'YXKWT0FKQM',
