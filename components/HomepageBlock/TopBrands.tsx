3fe2b5176813b9db72ae97d744bfc57226413bf6
vinod <vinod.2001@gmail.com>
2023-04-06 09:41:28 +0530

    topBrand carousel issue fixed

--- components/HomepageBlock/TopBrands.tsx
+++ components/HomepageBlock/TopBrands.tsx
@@ -7,31 +7,72 @@ import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { PortableText } from "@portabletext/react";
 
+function urlFor(source) {
+  return imageUrlBuilder(client).image(source);
+}
+
+const ptComponents = {
+  types: {
+    image: ({ value, index }) => {
+      if (!value?.asset?._ref) {
+        return null;
+      }
+      return (
+        <img
+          alt={value.alt || " "}
+          loading="lazy"
+          src={urlFor(value).fit("max").auto("format")}
+          width="100%"
+        />
+      );
+    },
+  },
+};
 
 export function TopBrands({ data }) {
   function urlFor(source: string) {
     return imageUrlBuilder(client).image(source);
   }
 
-  const ptComponents = {
-    types: {
-      image: ({ value, index }) => {
-        if (!value?.asset?._ref) {
-          return null;
-        }
-        return (
-          <img
-            alt={value.alt || " "}
-            loading="lazy"
-            src={urlFor(value).fit("max").auto("format").url()}
-            width="100%"
-          />
-        );
-      },
-    },
-  };
-
+ 
   const { contents, title, _key } = data || {};
+
+  // function displayItems() {
+  //   const isOdd = contents.length % 2;
+  //   if(isOdd){
+
+  //   }
+  // }
+
+  const sliderItems: number = contents.length > 2 ? 2 : contents.length;
+  const itemsContent: Array<any> = [];
+
+  for (let i = 0; i < contents.length; i += sliderItems) {
+    if (i % sliderItems === 0) {
+      itemsContent.push(
+        <Box
+          key={i.toString()}
+          sx={{
+            border: "0px solid",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {contents.slice(i, i + sliderItems).map((da, index) => {
+            // return <SubCategory key={index.toString()} item={da} />;
+            return (
+              <Box key={index.toString()} sx={{ border: "0px solid red" }}>
+                <PortableText value={da.image} components={ptComponents} />
+              </Box>
+            );
+          })}
+        </Box>
+      );
+    }
+  }
+
+  
   return (
     <Box>
       {contents && (
@@ -81,7 +122,9 @@ export function TopBrands({ data }) {
                 width: "80%",
               }}
             >
-              <Carousel
+              
+                
+                  <Carousel
                 sx={{ width: "100%" }}
                 PrevIcon={<ArrowBackIosNewIcon />}
                 NextIcon={<ArrowForwardIosIcon />}
@@ -106,24 +149,20 @@ export function TopBrands({ data }) {
                   },
                 }}
               >
-                <Box
-                  sx={{
-                    border: "0px solid",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  {contents.map((item, index) => (
+                  {/* {contents.map((item, index) => (
                     <Box key={item._key}>
                       <PortableText
                         value={item.image}
                         components={ptComponents}
                       />
                     </Box>
-                  ))}
-                </Box>
-              </Carousel>
+                  ))} */}
+                  {/* {itemsContent.map((item, index) => (
+                    <>{item}</>
+                  ))} */}
+                  {itemsContent}
+                  </Carousel>
+              
             </Box>
           </Box>
         </Box>
