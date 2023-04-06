987b8c754220244519472d58c02b35fa2d18a36b
vinod <vinod.2001@gmail.com>
2023-04-06 12:24:48 +0530

    Order confirmation update

--- pages/[locale]/order/index.tsx
+++ pages/[locale]/order/index.tsx
@@ -13,16 +13,158 @@ import {
   RootCategoriesDocument,
   OrderDetailsQuery, OrderDetailsQueryByIdDocument, OrderDetailsQueryByIdVariables
 } from "@/saleor/api";
+import {
+  Box, Button,
+  Grid, styled,Typography
+} from "@mui/material";
+import Divider from "@mui/material/Divider";
+import { Roboto } from "@next/font/google";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { useRegions } from "@/components/RegionsProvider";
+import VerifiedIcon from '@mui/icons-material/Verified';
 
 function OrderConfirmation({orderDetails}) {
   console.log(orderDetails, "orderDetails")
   const paths = usePaths();
-  return <div>
-    <div className="font-semibold text-3xl">Your order is completed!</div>
-    <p className="mt-2">
-      <Link href={paths.$url()}>Go back to homepage</Link>
-    </p>
-  </div>;
+  const { currentChannel, formatPrice, query } = useRegions();
+  return (
+    <Grid sx={{  border:'0px solid #f6f6f6', }}>
+      <Grid container xs={12}>
+        <Box  sx={{ m: 5,  width:'100%', display:'flex', justifyContent:'center', fontSize:'4.5rem' }}>
+          <Box>
+            <Box sx={{textAlign:'center'}}><VerifiedIcon sx={{color:'#F7961C'}}/></Box>
+            <Box sx={{textAlign:'center'}}>
+              <Typography sx={{color:'#F7961C', fontSize:'26px'}}>
+                Congratulations
+              </Typography>
+            </Box>
+            <Box sx={{mt:2}}>
+              <Typography sx={{color:'#343434', fontSize:'18px'}}>
+                Your Order is Submitted
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Grid>
+      <Grid container xs={12} sx={{ color: "#343434",}}>
+        <Box  sx={{ m: 5, backgroundColor: "#f6f6f6", p: 3, width:'100%' }}>
+          <Box sx={{mt:2,mb:2}}>
+            {/* <Box className="font-semibold text-3xl">Your order is completed!</Box> */}
+              <Typography sx={{fontSize:'20px',fontWeight:'bold'}}>
+                Order Number
+              </Typography>
+              <Typography>
+                {orderDetails.number}
+              </Typography>
+            {/* <p className="mt-2">
+              <Link href={paths.$url()}>Go back to homepage</Link>
+            </p> */}
+          </Box>
+          <Divider/>
+          <Box sx={{mt:2,mb:2}}>
+            <Box sx={{fontSize:'20px',fontWeight:'bold',mb:2}}>Shipping</Box>
+              <Typography>
+                <Box component="span">{orderDetails.shippingAddress.firstName}</Box>
+                <Box component="span" sx={{ml:1}}>{orderDetails.shippingAddress.lastName}</Box>
+              </Typography>
+              <Typography>
+                {orderDetails.shippingAddress.streetAddress1}
+              </Typography>
+              <Typography>
+                {orderDetails.shippingAddress.phone}
+              </Typography>
+          </Box>
+          <Divider/>
+          <Box sx={{mt:2,mb:2}}>
+            <Box sx={{fontSize:'20px',fontWeight:'bold',mb:2}}>Payment Details</Box>
+              <Typography>
+                <Box component="span">{orderDetails.shippingAddress.firstName}</Box>
+                <Box component="span" sx={{ml:1}}>{orderDetails.shippingAddress.lastName}</Box>
+              </Typography>
+              <Typography>
+                {orderDetails.shippingAddress.phone}
+              </Typography>
+          </Box>
+        </Box>
+      </Grid>
+
+      <Grid xs={4} sx={{ color: "#343434" }}>
+      <Box sx={{ backgroundColor: "#fff", p: 5 }}>
+        <Box sx={{ fontSize: "24px", mb: 3 }}>Order Summary</Box>
+        <Box>
+          <TableContainer>
+            <Table sx={{ minWidth: 250 }} aria-label="simple table">
+              <TableBody>
+                <TableRow
+                  sx={{
+                    "&:last-child td, &:last-child th": { border: 0 },
+                  }}
+                >
+                  <TableCell sx={{ pl: 0, fontSize: "18px" }}>
+                    Subtotal
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      fontSize: "18px",
+                      color: "inherit",
+                      textAlign: "right",
+                    }}
+                  >
+                    <b>{formatPrice(orderDetails?.subtotal?.net)}</b>
+                  </TableCell>
+                </TableRow>
+                {orderDetails?.total?.gross?.amount > 0 && (
+                  <TableRow
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    <TableCell sx={{ pl: 0, fontSize: "18px" }}>
+                      Delivery Cost
+                    </TableCell>
+                    <TableCell
+                      sx={{
+                        fontSize: "18px",
+                        color: "inherit",
+                        textAlign: "right",
+                      }}
+                    >
+                      <b>{formatPrice(orderDetails?.shippingPrice?.gross)}</b>
+                    </TableCell>
+                  </TableRow>
+                )}
+
+                <TableRow
+                  sx={{
+                    "&:last-child td, &:last-child th": { border: 0 },
+                  }}
+                >
+                  <TableCell sx={{ pl: 0, fontSize: "18px" }}>
+                    Order Total
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      fontSize: "18px",
+                      color: "inherit",
+                      textAlign: "right",
+                    }}
+                  >
+                    <b>{formatPrice(orderDetails?.total?.gross)}</b>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Box>
+    </Grid>
+    </Grid>
+  )
 }
 
 export default OrderConfirmation;
