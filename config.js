d14928aee768365ad8f8e9b9e77669fc959231d9
anooppius-bbcommerce <127185335+anooppius-bbcommerce@users.noreply.github.com>
2023-09-09 23:11:11 +0530

    Updated Index name

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const config = {
       apiVersion: process.env.NEXT_SANITY_STUDIO_API_VERSION || '2021-06-07',
       token: process.env.NEXT_SANITY_STUDIO_API_READ_TOKEN || 'skLeItChoOctyWwqWOuFb3YGEzGeuKKBipXBycgQb8zN5XTjxJrSYrSp0D8E2xGJ36Z4oXpqwRuTHASzchMRnNiasgqp2EOqUQSxNnYtOwNKSV9tYQLEJqSx8I53i6APwqvbtUeqKpUbQcOjDMuQAAFhd99dQdcmZRav7K97RzGFfFza3mJx',
     },
-    algoliaIndexName: process.env.NEXT_ALGOLIA_INDEX_NAME || 'test.mobile-app.AED.products',
+    algoliaIndexName: process.env.NEXT_ALGOLIA_INDEX_NAME || 'test.filli-uae-accadamic-city.AED.products',
     algoliaProjectId: process.env.NEXT_ALGOLIA_PROJECT_ID || 'YXKWT0FKQM',
     algoliaReadKey: process.env.NEXT_ALGOLIA_READ_KEY || '11d7ffaf5f69d91c4b7945038aeaef79',
   };
